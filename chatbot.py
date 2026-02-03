79d10bab94779daf587b4593bbc19514b469e881
sabihatm <sabihatameem@titan.co.in>
2026-02-03 11:25:22 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -10,11 +10,6 @@ import time
 import os
 
 app = Flask(__name__)
-try:
-    load_excel()
-    load_pincode_csv()
-except Exception as e:
-    write_log(f"Startup error: {e}", True)
 
 CORS(app)
 
@@ -372,3 +367,4 @@ def serve_files(filename):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
